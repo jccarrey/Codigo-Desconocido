cfe7cb78f31d7ee648d3385da3795da8eab3b82e
crxxz <tfuecru2003@ieszaidinvergeles.org>
2024-04-17 13:04:26 +0200

    Docker flask

--- docker/flask/app.py
+++ docker/flask/app.py
@@ -2,9 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def helloworld():
-    return '¡Hola, Mundo!'
+def hello_world():
+    return 'Hello, World!'
 
-
-if __name__ == '_main':
-    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
